161bd1bc05edea2f47522683e46d6f47c28bfa95
MarcoAOliari <marcoaoliari@gmail.com>
2021-10-19 15:01:40 -0300

    correcoes deploy

--- dist/server.js
+++ dist/server.js
@@ -1,5 +1,5 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _appjs = require('./app.js'); var _appjs2 = _interopRequireDefault(_appjs);
 
-const PORT = process.env.APP_PORT;
+const PORT = 3001;
 
 _appjs2.default.listen(PORT);
